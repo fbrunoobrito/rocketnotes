75edcf6082802da7b3f55d8574ae54c5d90ed077
fbrunoobrito <brunoarphenia@outlook.com>
2023-04-26 23:42:26 -0300

    Profile add Image Component

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,6 +1,6 @@
-import { Container, Form } from "./styles";
+import { Container, Form, Avatar } from "./styles";
 
-import { FiArrowLeft, FiUser, FiMail, FiLock } from "react-icons/fi";
+import { FiArrowLeft, FiUser, FiMail, FiLock, FiCamera } from "react-icons/fi";
 
 import { Input } from "../../components/Input";
 import { Button } from "../../components/Button";
@@ -14,11 +14,26 @@ export function Profile() {
         </a>
       </header>
       <Form>
+        <Avatar>
+          <img
+            src="https://github.com/fbrunoobrito.png"
+            alt="Foto de Usuário"
+          />
+
+          <label htmlFor="avatar">
+            <FiCamera />
+            <input id="avatar" type="file" />
+          </label>
+        </Avatar>
+
         <Input icon={FiUser} type="text" placeholder="Login" />
+
         <Input icon={FiMail} type="email" placeholder="E-Mail" />
-        <br />
+
         <Input icon={FiLock} type="password" placeholder="Senha Atual" />
+
         <Input icon={FiLock} type="password" placeholder="Nova Senha" />
+
         <Button title="Salvar" />
       </Form>
     </Container>
