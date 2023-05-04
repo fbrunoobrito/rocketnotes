e4d58a8d1774e8bfa08a3f1129feedc93d487fb9
fbrunoobrito <brunoarphenia@outlook.com>
2023-05-03 21:35:38 -0300

    New Profile Page Front End Done

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,8 +1,9 @@
-import { Header } from "../../components/Header";
-import { Input } from "../../components/Input";
 import { NoteItem } from "../../components/NoteItem";
 import { TextArea } from "../../components/TextArea";
 import { Section } from "../../components/Section";
+import { Button } from "../../components/Button";
+import { Header } from "../../components/Header";
+import { Input } from "../../components/Input";
 
 import { Container, Form } from "./styles";
 
@@ -25,6 +26,13 @@ export function New() {
             />
             <NoteItem isNew placeholder="Novo Link" />
           </Section>
+          <Section title="Marcadores">
+            <div className="tags">
+              <NoteItem value="React" />
+              <NoteItem isNew placeholder="Nova Tag" />
+            </div>
+          </Section>
+          <Button title="Salvar" />
         </Form>
       </main>
     </Container>
